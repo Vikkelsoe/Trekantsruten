1969a2ba4dbeea7b4426df1c9939bca90c10afc8
Nicklas Kofoed Larsen <nicklasklarsen@gmail.com>
2021-04-15 10:57:16 +0200

    b

---
+++ Rejsen_Pa_Trekantsruten/Assets/Scripts/Jigsaw_Movement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Jigsaw_Movement : MonoBehaviour
+{
+    public string pieceStatus = "idle";
+    public bool checkPlacement = false;
+    AudioSource audioPlaced;
+
+    void Start()
+    {
+        audioPlaced = this.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pieceStatus == "pickedUp")
+        {
+            // mousePosition gets the x and y coordinates of the mouse courser
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            // objectPosition translates the mousePosition into a position in the game
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            // Sets the position of this object to that of objPosition
+            transform.position = objPosition;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if ((other.gameObject.name == gameObject.name) && (checkPlacement == true))
+        {
+            other.GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+            transform.position = other.gameObject.transform.position;
+            pieceStatus = "locked";
+        }
+        if (pieceStatus == "locked" && checkPlacement == true)
+        {
+            checkPlacement = false;
+            GameManager.lockedPieces++;
+            audioPlaced.Play();
+            this.transform.position += new Vector3(0, 0, 0.01f);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        pieceStatus = "pickedUp";
+        checkPlacement = false;
+    }
+    private void OnMouseUp()
+    {
+        pieceStatus = "idle";
+        checkPlacement = true;
+    }
+}
---
+++ Rejsen_Pa_Trekantsruten/Assets/Scripts/Labyrint_Player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Labyrint_Player : MonoBehaviour
+{
+    public GameObject door;
+    public Text winText;
+    public GameObject winPanel;
+    public float speed = 2f;
+    Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(string button)
+    {
+        switch (button)
+        {
+            case "up":
+                rb.velocity = new Vector2(0, 1 * speed);
+                break;
+            case "down":
+                rb.velocity = new Vector2(0, -1 * speed);
+                break;
+            case "left":
+                rb.velocity = new Vector2(-1 * speed, 0);
+                break;
+            case "right":
+                rb.velocity = new Vector2(1 * speed, 0);
+                break;
+            case "stop":
+                rb.velocity = new Vector2(0, 0);
+                break;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Key")
+        {
+            Destroy(collision.gameObject);
+            Destroy(door);
+        }
+
+        if (collision.gameObject.tag == "Enemies")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (collision.gameObject.tag == "Goal")
+        {
+            Destroy(collision.gameObject);
+            winText.text = "Flot klaret!";
+            winPanel.SetActive(true);
+            Debug.Log("DU HAR VUNDET!");
+        }
+    }
+}
